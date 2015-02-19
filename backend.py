fea74eaaab7e4ad8f4b7d9b79890da9f56b88b65
jasonvivier <jason.a.vivier@gmail.com>
2015-02-19 12:08:50 -0500

    thinking

--- backend.py
+++ backend.py
@@ -1,2 +1,13 @@
 #!/bin/python
 
+#if not MAIDSAFE
+	#Generate random id
+	#store random_id in var
+	#display random_id in HTML for user to share with others
+#if MAIDSAFE
+	#Figure out how to pull API id. 
+	#assign that user access to shared storage
+	#create temporary storage token. 
+	#look for other tokens and findfriends()
+	#synchronize storage with all clients
+	
